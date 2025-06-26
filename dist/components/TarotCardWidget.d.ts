8a13640098fe6e3989f5f11d03c6f94ffe3b423d
Jason Tiller <jktiller86@gmail.com>
2025-06-26 12:11:27 +1200

    Add dist/ to gitignore and remove build artifacts from tracking

--- dist/components/TarotCardWidget.d.ts
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-interface Props {
-    subscribeEndpoint: string;
-}
-declare const TarotCardWidget: React.FC<Props>;
-export default TarotCardWidget;
--- dist/mount.d.ts
+++
@@ -1,6 +0,0 @@
-type InitOptions = {
-    root: string;
-    subscribeEndpoint: string;
-};
-export declare function init(options: InitOptions): void;
-export {};
